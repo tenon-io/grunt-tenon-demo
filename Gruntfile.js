1e06bd36be715a3caa6c6a8af846b0e5b6954338
Karl Groves <karl@karlgroves.com>
2016-03-04 17:39:48 -0500

    Adds an ngrok task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,12 @@
 'use strict';
 
+var ngrok = require('ngrok');
+var unirest = require('unirest');
+var jsonfile = require('jsonfile');
+var util = require('util');
+var fs = require('fs');
+var glob = require('glob-fs')({ gitignore: true });
+
 module.exports = function (grunt) {
     // Load all grunt tasks
     require('load-grunt-tasks')(grunt);
@@ -16,6 +23,20 @@ module.exports = function (grunt) {
 
         clean: {
             files: ['dist/*']
+        },
+
+        /* Serve up the project on localhost */
+        connect: {
+            server: {
+                options: {
+                    protocol: 'http',
+                    port: 9001,
+                    hostname: '*',
+                    base: 'src',
+                    directory: 'src',
+                    keepalive: true
+                }
+            }
         },
 
         /* Run JSHint on our JS files */
@@ -92,12 +113,6 @@ module.exports = function (grunt) {
             },
         },
 
-        shell: {
-            dirListing: {
-                command: 'ls'
-            }
-        },
-
         /* Watch tasks */
         watch: {
             gruntfile: {
@@ -115,6 +130,78 @@ module.exports = function (grunt) {
         }
     });
 
+
+
+    grunt.registerTask('ngrok', 'Test the project URLs on localhost', function() {
+        var done = this.async();
+        var config =  JSON.parse(require('fs').readFileSync('.tenonrc', 'utf8'));
+
+        ngrok.connect({
+            addr: '9001', // port or network address
+            authtoken: ' ' // your authtoken from ngrok.com
+        }, function (err, url) {
+
+            if (err !== null) {
+                grunt.fail.fatal(err);
+                return done();
+            }
+
+            grunt.log.writeln('ngrok URL: ' + url);
+
+            // do the loop-de-doo on all the project files
+            var filenames, i, testUrl;
+
+            filenames = glob.readdirSync('/**/*.html');
+            for (i = 0; i < filenames.length; i++) {
+
+                testUrl = url + '/' + filenames[i].replace('src/', '');
+
+                grunt.log.writeln('Test Url: ' + testUrl);
+
+                unirest.post(config.apiURL).send({
+                    key: config.key,
+                    url: testUrl,
+                    projectID: config.projectID,
+                    certainty: config.certainty,
+                    level: config.level,
+                    priority: config.priority,
+                    ref: config.ref,
+                    store: config.store
+                }).header('Accept', 'application/json').end(function (response) {
+                    grunt.log.writeln('Testing: ' + testUrl);
+
+                    grunt.log.writeln('Status: ' + response.status);
+
+                    if (response.status > 200) {
+                        grunt.fail.fatal('Tenon API response: ' + response.status);
+                        return done();
+                    }
+
+                    var data = JSON.parse(JSON.stringify(response.body));
+                    grunt.log.writeln('responseID: ' + data.request.responseID);
+                    grunt.log.writeln('Total Errors: ' + data.resultSummary.issues.totalErrors);
+                    grunt.log.writeln('Total Warnings: ' + data.resultSummary.issues.totalWarnings);
+
+                    var file = 'tmp/' + data.request.responseID + '.json';
+
+                    jsonfile.writeFile(file, data, function (err) {
+                        if (err) {
+                            grunt.log.writeln('Error Writing File: ' + err);
+                            grunt.log.writeln('--------------------');
+                        }
+                        else {
+                            grunt.log.writeln('Response Saved To: ' + file);
+                            grunt.log.writeln('--------------------');
+                        }
+                    });
+
+                });
+            }
+
+        });
+    });
+
     // Default task.
+    grunt.registerTask('tenonLive', ['connect', 'ngrok']);
     grunt.registerTask('default', ['jshint', 'jsonlint', 'tenon']);
 };
